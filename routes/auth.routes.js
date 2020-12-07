9beefadf8b6a8c96ff7b1546392ce81ff11402f4
balbatushka <balbatushka@gmail.com>
2020-12-07 17:59:24 +0300

    client(front) pages with routing added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,29 +1,86 @@
 const { Router, response } = require("express");
 const User = require("../models/User");
+const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const iwt = require("jsonwebtoken");
+const config = require("./../config/default.json");
 const router = Router();
 
 //  /api/auth/register
-router.post("/register", async (req, res) => {
-  try {
-    const { email, password } = req.body;
+router.post(
+  "/register",
+  [
+    check("email", "Wrong email").isEmail(),
+    check("password", "Min password length is 6 symbols").isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
 
-    const candidate = await User.findOne({ email });
+      if (errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "Not correct data for registration",
+        });
+      }
+      const { email, password } = req.body;
 
-    if (candidate) {
-      return res.status(400).json({ message: "This user already exists " });
+      const candidate = await User.findOne({ email });
+
+      if (candidate) {
+        return res.status(400).json({ message: "This user already exists " });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new User({ email: email, password: hashedPassword });
+      await user.save();
+      res.status(201).json({ message: "User creared" });
+    } catch (e) {
+      res.status(500).json({ message: "Somethhing went wrong ..." });
     }
-
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const user = new User({ email: email, password: hashedPassword });
-    await user.save();
-    res.status(201).json({ message: "User creared" });
-  } catch (e) {
-    res.status(500).json({ message: "Somethhing went wrong ..." });
   }
-});
+);
 
 //  /api/auth/login
-router.post("/login", async (req, res) => {});
+router.post(
+  "/login",
+  [
+    check("email", "Enter correct email").normalizeEmail().isEmail(),
+    check("password", "Enter password").exists(),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      if (errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "Not correct data for login",
+        });
+      }
+
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ message: "No such user" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ message: "Wrong password" });
+      }
+
+      const token = jwt.sign({ userId: user.id }, config.get("jwtSecret"), {
+        expiresIn: "1h",
+      });
+
+      res.json({ token, userId: user.id });
+    } catch (e) {
+      res.status(500).json({ message: "Somethhing went wrong ..." });
+    }
+  }
+);
 
 module.exports = router;
