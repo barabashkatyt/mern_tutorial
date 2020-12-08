34e0a40d06796e1a181e54524ce1b9380b3c8310
balbatushka <balbatushka@gmail.com>
2020-12-08 09:16:01 +0300

    feat: styles authpage added

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -2,8 +2,43 @@ import React from "react";
 
 export const AuthPage = () => {
   return (
-    <div>
-      <h1>Auth Page</h1>
+    <div className='row'>
+      <div className='col s6 offset-s3'>
+        <h1>Make url shorter</h1>
+        <div className='card blue darken-1'>
+          <div className='card-content white-text'>
+            <span className='card-title'>Authorization</span>
+            <div>
+              <div className='input-field'>
+                <input
+                  placeholder='Enter email'
+                  id='email'
+                  type='text'
+                  name='email'
+                  className='yellow-input'
+                />
+                <label htmlFor='email'>Email</label>
+              </div>
+            </div>
+            <div className='input-field'>
+              <input
+                placeholder='Enter password'
+                id='password'
+                type='password'
+                name='password'
+                className='yellow-input'
+              />
+              <label htmlFor='email'>Password</label>
+            </div>
+          </div>
+          <div className='card-action'>
+            <button className='btn yellow darken-4' style={{ marginRight: 10 }}>
+              Login
+            </button>
+            <button className='btn grey lighten-1 black-text'>Register</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
