3d4e426d7c3d73c4c4ac581df7c9380a78833e94
balbatushka <balbatushka@gmail.com>
2020-12-10 11:05:38 +0300

    feat: authentication(login) made

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useHttp } from "../hooks/http.hook";
 import { useMessage } from "../hooks/message.hook";
+import { AuthContext } from "../context/AuthContext";
 
 export const AuthPage = () => {
+  const auth = useContext(AuthContext);
   const message = useMessage();
 
   const { loading, error, request, clearError } = useHttp();
@@ -21,6 +23,13 @@ export const AuthPage = () => {
     try {
       const data = await request("/api/auth/register", "POST", { ...form });
       message(data.message);
+    } catch (e) {}
+  };
+
+  const loginHandler = async () => {
+    try {
+      const data = await request("/api/auth/login", "POST", { ...form });
+      auth.login(data.token, data.userId);
     } catch (e) {}
   };
 
@@ -64,6 +73,7 @@ export const AuthPage = () => {
               className='btn yellow darken-4'
               style={{ marginRight: 10 }}
               disabled={loading}
+              onClick={loginHandler}
             >
               Login
             </button>
