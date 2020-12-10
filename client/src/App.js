3656c7f9ad84836f252d896ca97dbef8f88951ab
balbatushka <balbatushka@gmail.com>
2020-12-10 13:29:39 +0300

    feat: Navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { useRoutes } from "./routes";
 import { useAuth } from "./hooks/auth.hook";
 import { AuthContext } from "./context/AuthContext";
 import "materialize-css";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   const { token, login, logout, userId } = useAuth();
@@ -21,6 +22,7 @@ function App() {
       }}
     >
       <Router>
+        {isAuthenticated && <Navbar />}
         <div className='container'>{routes}</div>
       </Router>
     </AuthContext.Provider>
